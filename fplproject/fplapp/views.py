03f361e32fd25204ee0a06494afd51e2e54b72ba
sakriyaawal <65768040+sakriyaawal@users.noreply.github.com>
2023-07-29 14:11:55 +0545

    Update views.py

--- fplproject/fplapp/views.py
+++ fplproject/fplapp/views.py
@@ -7,7 +7,6 @@ base_url = "https://fantasy.premierleague.com/api/"
 
 # Create your views here.
 def home_page(request):
-    print('hello')
     return render(request, 'index.html')
 
 def Fixtures(request):
