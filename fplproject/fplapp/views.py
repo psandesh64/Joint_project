83e5c85bd977823c802a83f91c4d592ebdd61076
psandesh64 <psandesh64@gmail.com>
2023-07-29 13:51:47 +0545

    commit 2

--- fplproject/fplapp/views.py
+++ fplproject/fplapp/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+import requests
+import json
+
+base_url = "https://fantasy.premierleague.com/api/" 
 
 # Create your views here.
 def home_page(request):
     return render(request, 'index.html')
+
+def Fixtures(request):
+    fixtures_url = base_url + "fixtures/"
+    fixtures_response = requests.get(fixtures_url,verify = False).json()
+    return JsonResponse(fixtures_response,safe=False)
